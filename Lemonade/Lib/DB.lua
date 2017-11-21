be871f52404131d5973fc4f3428b7a47d181bff9
YellowDi <yellowdi@me.com>
2017-11-21 16:58:25 +0800

    init

---
+++ Lemonade/Lib/DB.lua
@@ -0,0 +1,7 @@
+﻿-- Engine
+local S, C, L, DB = unpack(select(2, ...))
+
+-- Begin
+DB.GlowTex = "InterFace\\AddOns\\Lemonade\\Media\\GlowTex"
+DB.Statusbar = "InterFace\\AddOns\\Lemonade\\Media\\Statusbar"
+DB.AuraFont = "Interface\\Addons\\Lemonade\\Media\\ROADWAY.TTF"
